818a485b2102e6a1455b53044b83fa19f055bbae
Rogerio Fonseca Goncalves <rogerio.fonseca@gmail.com>
2024-02-23 12:43:21 -0300

    Initial commit...

---
+++ src/test/java/br/com/virtualbooks/tabernabooks/TabernabooksApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.virtualbooks.tabernabooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TabernabooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
