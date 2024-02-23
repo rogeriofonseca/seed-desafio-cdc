23289b13ce95e43a850e79860b90844b910ffd23
Rogerio Fonseca Goncalves <rogerio.fonseca@gmail.com>
2024-02-23 12:43:21 -0300

    Initial commit...

---
+++ src/main/java/br/com/virtualbooks/tabernabooks/TabernabooksApplication.java
@@ -0,0 +1,13 @@
+package br.com.virtualbooks.tabernabooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TabernabooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TabernabooksApplication.class, args);
+	}
+
+}
