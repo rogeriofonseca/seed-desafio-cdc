9f4660dfec31d692ce545dd3608d71157d0efebd
Rogerio Fonseca Goncalves <rogerio.fonseca@gmail.com>
2024-02-29 23:54:54 -0300

    Necessidades: email do autor deve ser unico e nao pode haver duplicidade.

--- src/main/java/br/com/virtualbooks/tabernabooks/autor/AutorRepository.java
+++ src/main/java/br/com/virtualbooks/tabernabooks/autor/AutorRepository.java
@@ -2,5 +2,9 @@ package br.com.virtualbooks.tabernabooks.autor;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AutorRepository extends JpaRepository<Autor, Long> {
+
+    Optional<Autor> findByEmail(String email);
 }
---
+++ src/main/java/br/com/virtualbooks/tabernabooks/autor/ProibeEmailDuplicadoAutorValidator.java
@@ -0,0 +1,36 @@
+package br.com.virtualbooks.tabernabooks.autor;
+
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+import java.util.Optional;
+
+@Component
+public class ProibeEmailDuplicadoAutorValidator implements Validator {
+    private AutorRepository autorRepository;
+
+    public ProibeEmailDuplicadoAutorValidator(AutorRepository autorRepository) {
+        this.autorRepository = autorRepository;
+    }
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return AutorRequest.class.isAssignableFrom(clazz);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        if(errors.hasErrors()){
+            return;
+        }
+
+        AutorRequest request = (AutorRequest) target;
+        Optional<Autor> autorCadastrado = autorRepository.findByEmail(request.getEmail());
+        if(autorCadastrado.isPresent()){
+            String emailCadastrado = autorCadastrado.get().getEmail();
+            errors.reject("email", null, String.format("Email já cadastrado: %s", emailCadastrado));
+        }
+
+    }
+}
