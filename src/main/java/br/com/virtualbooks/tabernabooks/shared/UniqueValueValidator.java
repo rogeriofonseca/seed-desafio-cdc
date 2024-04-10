c6825e5c851239659d403febc7fb764a7f8d91ce
Rogerio Fonseca Goncalves <rogerio.goncalves@bbtecno.com.br>
2024-04-10 00:45:41 -0300

    Generic Validator.

---
+++ src/main/java/br/com/virtualbooks/tabernabooks/shared/UniqueValueValidator.java
@@ -0,0 +1,37 @@
+package br.com.virtualbooks.tabernabooks.shared;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import org.springframework.util.Assert;
+
+import java.util.List;
+
+public class UniqueValueValidator implements ConstraintValidator<UniqueValue, Object> {
+
+    private String domainAttribute;
+    private Class<?> klass;
+    @PersistenceContext
+    private EntityManager manager;
+
+    @Override
+    public void initialize(UniqueValue params) {
+        ConstraintValidator.super.initialize(params);
+        domainAttribute = params.fieldName();
+        klass = params.domainClass();
+    }
+
+    @Override
+    public boolean isValid(Object value, ConstraintValidatorContext context) {
+        Query query = manager.createQuery("select 1 from "+klass.getName()+" where "+domainAttribute+"=:value");
+        query.setParameter("value", value);
+        List<?> list = query.getResultList();
+        Assert.state(list.size() <=1, "Foi encontrado mais de um "+klass+" com o atributo "+domainAttribute+" = "+value);
+
+        return list.isEmpty();
+    }
+
+
+}
