7b2cdcfcc367710d08c115ad2f92b27c9b769564
Rogerio Fonseca Goncalves <rogerio.fonseca@gmail.com>
2024-04-13 00:54:43 -0300

    Inicializando autores e categoria no startup da aplicação.

--- src/main/java/br/com/virtualbooks/tabernabooks/TabernabooksApplication.java
+++ src/main/java/br/com/virtualbooks/tabernabooks/TabernabooksApplication.java
@@ -1,13 +1,33 @@
 package br.com.virtualbooks.tabernabooks;
 
+import br.com.virtualbooks.tabernabooks.autor.AutorRepository;
+import br.com.virtualbooks.tabernabooks.autor.AutorRequest;
+import br.com.virtualbooks.tabernabooks.categoria.CategoriaRepository;
+import br.com.virtualbooks.tabernabooks.categoria.CategoriaRequest;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TabernabooksApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TabernabooksApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(AutorRepository autorRepository, CategoriaRepository categoriaRepository) {
+		AutorRequest rogerio = new AutorRequest("Rogério Fonseca", "rogerio@gmail.com", "Author Tech tech");
+		AutorRequest alice = new AutorRequest("Melissa", "melissa@gmail.com", "Fiction");
+
+		CategoriaRequest categoria1 = new CategoriaRequest("Fiction");
+		return args -> {
+			autorRepository.save(rogerio.mapToModel());
+			autorRepository.save(alice.mapToModel());
+
+			categoriaRepository.save(categoria1.mapToModel());
+		};
 	}
 
 }
