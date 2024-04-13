bbecdb6c96112b8fe2d582dded7b8cb8b1a5047a
Rogerio Fonseca Goncalves <rogerio.goncalves@bbtecno.com.br>
2024-04-13 00:54:07 -0300

    Cadastro Livro necessidades Um título Um resumo do que vai ser encontrado no livro Um sumário de tamanho livre. O texto deve entrar no formato markdown, que é uma string. Dessa forma ele pode ser formatado depois da maneira apropriada. Preço do livro Número de páginas Isbn(identificador do livro) Data que ele deve entrar no ar(de publicação) Um livro pertence a uma categoria Um livro é de um autor restrições Título é obrigatório Título é único Resumo é obrigatório e tem no máximo 500 caracteres Sumário é de tamanho livre. Preço é obrigatório e o mínimo é de 20 Número de páginas é obrigatória e o mínimo é de 100 Isbn é obrigatório, formato livre Isbn é único Data que vai entrar no ar precisa ser no futuro A categoria não pode ser nula O autor não pode ser nulo
    resultado esperado
    Um novo livro precisa ser criado e status 200 retornado
    Caso alguma restrição não seja atendida, retorne 400 e um json informando os problemas de validação

---
+++ src/main/java/br/com/virtualbooks/tabernabooks/livro/Livro.java
@@ -0,0 +1,62 @@
+package br.com.virtualbooks.tabernabooks.livro;
+
+import br.com.virtualbooks.tabernabooks.autor.Autor;
+import br.com.virtualbooks.tabernabooks.categoria.Categoria;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Livro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String titulo;
+
+    @NotBlank
+    @Size(max = 500)
+    private String resumoLivro;
+    private String sumario;
+    @NotBlank
+    @Size(min = 20)
+    private BigDecimal preco;
+    @NotBlank
+    @Positive
+    @Size(min = 100)
+    private Integer numeroPaginas;
+    @NotBlank
+    private String isbn;
+    @FutureOrPresent
+    private LocalDate dataPublicacao;
+    @NotBlank
+    @OneToOne
+    @PrimaryKeyJoinColumn
+    private Categoria categoria;
+    @NotNull
+    @OneToOne
+    @PrimaryKeyJoinColumn
+    private Autor autor;
+
+    public Livro(String titulo, String resumoLivro, String sumario, BigDecimal preco, Integer numeroPaginas, String isbn, LocalDate dataPublicacao, Categoria idCategoria, Autor idAutor) {
+        this.titulo = titulo;
+        this.resumoLivro = resumoLivro;
+        this.sumario = sumario;
+        this.preco = preco;
+        this.numeroPaginas = numeroPaginas;
+        this.isbn = isbn;
+        this.dataPublicacao = dataPublicacao;
+        this.categoria = idCategoria;
+        this.autor = idAutor;
+    }
+}
---
+++ src/main/java/br/com/virtualbooks/tabernabooks/livro/NovoLivroRequest.java
@@ -0,0 +1,81 @@
+package br.com.virtualbooks.tabernabooks.livro;
+
+import br.com.virtualbooks.tabernabooks.autor.Autor;
+import br.com.virtualbooks.tabernabooks.autor.AutorRepository;
+import br.com.virtualbooks.tabernabooks.categoria.Categoria;
+import br.com.virtualbooks.tabernabooks.categoria.CategoriaRepository;
+import br.com.virtualbooks.tabernabooks.shared.UniqueValue;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Optional;
+
+@Data
+public class NovoLivroRequest {
+
+    private AutorRepository autorRepository;
+    private CategoriaRepository categoriaRepository;
+
+    public NovoLivroRequest(AutorRepository autorRepository, CategoriaRepository categoriaRepository) {
+        this.autorRepository = autorRepository;
+        this.categoriaRepository = categoriaRepository;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @UniqueValue(fieldName = "titulo", domainClass = Livro.class)
+    private String titulo;
+
+    @NotBlank
+    @Size(max = 500)
+    private String resumoLivro;
+    private String sumario;
+
+    @NotNull
+    @Min(20)
+    private BigDecimal preco;
+
+    @Min(100)
+    private Integer numeroPaginas;
+
+    @NotBlank
+    private String isbn;
+    @FutureOrPresent
+    @JsonFormat(pattern = "dd/MM/yyyy", shape = JsonFormat.Shape.STRING)
+    private LocalDate dataPublicacao;
+    @NotNull
+    private Long idCategoria;
+    @NotNull
+    private Long idAutor;
+
+//    public NovoLivroRequest(AutorRepository autorRepository, CategoriaRepository categoriaRepository) {
+//        this.autorRepository = autorRepository;
+//        this.categoriaRepository = categoriaRepository;
+//    }
+
+    public Livro mapToModel(){
+        Autor autor = autorRepository.findById(this.getIdAutor()).orElse(new Autor("Noehaus", "rogerio@gmail.com","Descricao qualquer"));
+        Categoria categoria = categoriaRepository.findById(this.getIdCategoria()).get();
+        return new Livro(
+                this.getTitulo(),
+                this.getResumoLivro(),
+                this.getSumario(),
+                this.getPreco(),
+                this.getNumeroPaginas(),
+                this.getIsbn(),
+                this.getDataPublicacao(),
+                categoria,
+                autor
+        );
+    }
+
+}
